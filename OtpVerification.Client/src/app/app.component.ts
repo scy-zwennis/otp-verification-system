bae162ac31f2913dc65db21515f076437297c837
Scy Zwennis <scyzwennis@gmail.com>
2025-02-13 18:22:55 +0000

    Remove navbar

--- OtpVerification.Client/src/app/app.component.ts
+++ OtpVerification.Client/src/app/app.component.ts
@@ -1,14 +1,13 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, RouterModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'OtpVerification.Client';
+  title = 'OTP Verification';
 }
